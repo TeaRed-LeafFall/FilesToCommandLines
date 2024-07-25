bb90f76ab990bcb8cb22805c5e805bfff75f3cfc
ChengSiXi <1423360499@qq.com>
2024-07-26 00:46:15 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace FilesToCommandLines;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        components = new System.ComponentModel.Container();
+        tableLayoutPanel1 = new TableLayoutPanel();
+        tableLayoutPanel2 = new TableLayoutPanel();
+        openFolder = new Button();
+        updateList = new Button();
+        generateText = new Button();
+        searchBox = new TextBox();
+        templateBox = new TextBox();
+        pathLabel = new Label();
+        filesListBox = new CheckedListBox();
+        contextMenuStrip = new ContextMenuStrip(components);
+        全选ToolStripMenuItem = new ToolStripMenuItem();
+        全不选ToolStripMenuItem = new ToolStripMenuItem();
+        反选ToolStripMenuItem = new ToolStripMenuItem();
+        folderBrowserDialog = new FolderBrowserDialog();
+        toolTip1 = new ToolTip(components);
+        tableLayoutPanel1.SuspendLayout();
+        tableLayoutPanel2.SuspendLayout();
+        contextMenuStrip.SuspendLayout();
+        SuspendLayout();
+        // 
+        // tableLayoutPanel1
+        // 
+        tableLayoutPanel1.ColumnCount = 1;
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 24F));
+        tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
+        tableLayoutPanel1.Controls.Add(filesListBox, 0, 1);
+        tableLayoutPanel1.Dock = DockStyle.Fill;
+        tableLayoutPanel1.Location = new Point(0, 0);
+        tableLayoutPanel1.Margin = new Padding(4, 3, 4, 3);
+        tableLayoutPanel1.Name = "tableLayoutPanel1";
+        tableLayoutPanel1.RowCount = 2;
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+        tableLayoutPanel1.Size = new Size(446, 469);
+        tableLayoutPanel1.TabIndex = 0;
+        // 
+        // tableLayoutPanel2
+        // 
+        tableLayoutPanel2.ColumnCount = 2;
+        tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
+        tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+        tableLayoutPanel2.Controls.Add(openFolder, 1, 0);
+        tableLayoutPanel2.Controls.Add(updateList, 1, 1);
+        tableLayoutPanel2.Controls.Add(generateText, 1, 2);
+        tableLayoutPanel2.Controls.Add(searchBox, 0, 1);
+        tableLayoutPanel2.Controls.Add(templateBox, 0, 2);
+        tableLayoutPanel2.Controls.Add(pathLabel, 0, 0);
+        tableLayoutPanel2.Dock = DockStyle.Fill;
+        tableLayoutPanel2.Location = new Point(4, 3);
+        tableLayoutPanel2.Margin = new Padding(4, 3, 4, 3);
+        tableLayoutPanel2.Name = "tableLayoutPanel2";
+        tableLayoutPanel2.RowCount = 3;
+        tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+        tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+        tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+        tableLayoutPanel2.Size = new Size(438, 87);
+        tableLayoutPanel2.TabIndex = 0;
+        // 
+        // openFolder
+        // 
+        openFolder.Dock = DockStyle.Fill;
+        openFolder.Location = new Point(354, 3);
+        openFolder.Margin = new Padding(4, 3, 4, 3);
+        openFolder.Name = "openFolder";
+        openFolder.Size = new Size(80, 23);
+        openFolder.TabIndex = 0;
+        openFolder.Text = "打开目录";
+        openFolder.UseVisualStyleBackColor = true;
+        openFolder.Click += openFolder_Click;
+        // 
+        // updateList
+        // 
+        updateList.Dock = DockStyle.Fill;
+        updateList.Location = new Point(353, 32);
+        updateList.Name = "updateList";
+        updateList.Size = new Size(82, 23);
+        updateList.TabIndex = 1;
+        updateList.Text = "刷新列表";
+        updateList.UseVisualStyleBackColor = true;
+        updateList.Click += updateList_Click;
+        // 
+        // generateText
+        // 
+        generateText.Dock = DockStyle.Fill;
+        generateText.Location = new Point(353, 61);
+        generateText.Name = "generateText";
+        generateText.Size = new Size(82, 23);
+        generateText.TabIndex = 2;
+        generateText.Text = "生成文本";
+        generateText.UseVisualStyleBackColor = true;
+        generateText.Click += generateText_Click;
+        // 
+        // searchBox
+        // 
+        searchBox.Dock = DockStyle.Fill;
+        searchBox.Location = new Point(3, 32);
+        searchBox.Name = "searchBox";
+        searchBox.PlaceholderText = "填写比如 *.png 样式的文本 支持?和*占位符";
+        searchBox.Size = new Size(344, 25);
+        searchBox.TabIndex = 3;
+        toolTip1.SetToolTip(searchBox, "可以使用 搜索功能过滤文件");
+        // 
+        // templateBox
+        // 
+        templateBox.Dock = DockStyle.Fill;
+        templateBox.Location = new Point(3, 61);
+        templateBox.Name = "templateBox";
+        templateBox.PlaceholderText = "模板 类似 xx  %F  可以用%N仅获取没有扩展名的文件名";
+        templateBox.Size = new Size(344, 25);
+        templateBox.TabIndex = 4;
+        toolTip1.SetToolTip(templateBox, "例如 ffmpeg -i %F %N.wav \r\n%F 文件的完整路径包含文件名及扩展名\r\n%N 只有文件名，没有扩展名和路径\r\n请需要你在想要保存的路径执行\r\n建议%F两侧加上双引号以免具有特殊问题");
+        // 
+        // pathLabel
+        // 
+        pathLabel.AutoSize = true;
+        pathLabel.Dock = DockStyle.Fill;
+        pathLabel.Location = new Point(3, 0);
+        pathLabel.Name = "pathLabel";
+        pathLabel.Size = new Size(344, 29);
+        pathLabel.TabIndex = 5;
+        pathLabel.Text = "请选择一个目录";
+        pathLabel.TextAlign = ContentAlignment.MiddleLeft;
+        // 
+        // filesListBox
+        // 
+        filesListBox.BorderStyle = BorderStyle.None;
+        filesListBox.CheckOnClick = true;
+        filesListBox.ContextMenuStrip = contextMenuStrip;
+        filesListBox.Dock = DockStyle.Fill;
+        filesListBox.FormattingEnabled = true;
+        filesListBox.Location = new Point(3, 96);
+        filesListBox.Name = "filesListBox";
+        filesListBox.Size = new Size(440, 370);
+        filesListBox.TabIndex = 1;
+        toolTip1.SetToolTip(filesListBox, "这里是选中文件夹的文件列表，右键可以打开选择菜单。\r\n请注意这里不包含子文件夹的文件，只有这一层的！");
+        // 
+        // contextMenuStrip
+        // 
+        contextMenuStrip.Items.AddRange(new ToolStripItem[] { 全选ToolStripMenuItem, 全不选ToolStripMenuItem, 反选ToolStripMenuItem });
+        contextMenuStrip.Name = "contextMenuStrip";
+        contextMenuStrip.Size = new Size(118, 76);
+        contextMenuStrip.Text = "对 文件列表 进行选择操作";
+        // 
+        // 全选ToolStripMenuItem
+        // 
+        全选ToolStripMenuItem.Name = "全选ToolStripMenuItem";
+        全选ToolStripMenuItem.Size = new Size(117, 24);
+        全选ToolStripMenuItem.Text = "全选";
+        全选ToolStripMenuItem.Click += 全选ToolStripMenuItem_Click;
+        // 
+        // 全不选ToolStripMenuItem
+        // 
+        全不选ToolStripMenuItem.Name = "全不选ToolStripMenuItem";
+        全不选ToolStripMenuItem.Size = new Size(117, 24);
+        全不选ToolStripMenuItem.Text = "全不选";
+        全不选ToolStripMenuItem.Click += 全不选ToolStripMenuItem_Click;
+        // 
+        // 反选ToolStripMenuItem
+        // 
+        反选ToolStripMenuItem.Name = "反选ToolStripMenuItem";
+        反选ToolStripMenuItem.Size = new Size(117, 24);
+        反选ToolStripMenuItem.Text = "反选";
+        反选ToolStripMenuItem.Click += 反选ToolStripMenuItem_Click;
+        // 
+        // folderBrowserDialog
+        // 
+        folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+        folderBrowserDialog.ShowNewFolderButton = false;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 17F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(446, 469);
+        Controls.Add(tableLayoutPanel1);
+        Font = new Font("Resource Han Rounded CN", 9F, FontStyle.Regular, GraphicsUnit.Point, 134);
+        Margin = new Padding(4, 3, 4, 3);
+        MinimumSize = new Size(462, 507);
+        Name = "Form1";
+        Text = "Files To CommandLines ";
+        tableLayoutPanel1.ResumeLayout(false);
+        tableLayoutPanel2.ResumeLayout(false);
+        tableLayoutPanel2.PerformLayout();
+        contextMenuStrip.ResumeLayout(false);
+        ResumeLayout(false);
+    }
+
+    #endregion
+
+    private TableLayoutPanel tableLayoutPanel1;
+    private TableLayoutPanel tableLayoutPanel2;
+    private Button openFolder;
+    private Button updateList;
+    private Button generateText;
+    private TextBox searchBox;
+    private TextBox templateBox;
+    private Label pathLabel;
+    private CheckedListBox filesListBox;
+    private FolderBrowserDialog folderBrowserDialog;
+    private ContextMenuStrip contextMenuStrip;
+    private ToolStripMenuItem 全选ToolStripMenuItem;
+    private ToolStripMenuItem 反选ToolStripMenuItem;
+    private ToolStripMenuItem 全不选ToolStripMenuItem;
+    private ToolTip toolTip1;
+}
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace FilesToCommandLines;
+
+public partial class Form1 : Form
+{
+    string path = string.Empty;
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void openFolder_Click(object sender, EventArgs e)
+    {
+        folderBrowserDialog.ShowDialog();
+        var path = folderBrowserDialog.SelectedPath;
+        if (Path.Exists(path))
+        {
+            this.path = path;
+            OnUpdatePath();
+        }
+    }
+    private void OnUpdatePath()
+    {
+        pathLabel.Text = path;
+        OnUpdateFiles();
+    }
+    private void OnUpdateFiles()
+    {
+        if (Path.Exists(path))
+        {
+            filesListBox.BeginUpdate();
+            filesListBox.Items.Clear();
+            var filePaths = Directory.GetFiles(path, searchBox.Text);
+            List<string> fileNames = new();
+            foreach (var filePath in filePaths)
+            {
+                fileNames.Add(Path.GetFileName(filePath));
+            }
+            filesListBox.Items.AddRange(fileNames.ToArray());
+            filesListBox.EndUpdate();
+        }
+        else
+        {
+            MessageBox.Show("你似乎没有选择目录，或者目录现在不存在了！", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void updateList_Click(object sender, EventArgs e)
+    {
+        OnUpdateFiles();
+    }
+
+    private void 全选ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        filesListBox.BeginUpdate();
+        for (var i = 0; i < filesListBox.Items.Count; i++)
+        {
+            filesListBox.SetItemChecked(i, true);
+        }
+        filesListBox.EndUpdate();
+    }
+
+    private void 反选ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        filesListBox.BeginUpdate();
+        for (var i = 0; i < filesListBox.Items.Count; i++)
+        {
+            filesListBox.SetItemChecked(i, filesListBox.GetItemChecked(i));
+        }
+        filesListBox.EndUpdate();
+    }
+
+    private void 全不选ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        filesListBox.BeginUpdate();
+        for (var i = 0; i < filesListBox.Items.Count; i++)
+        {
+            filesListBox.SetItemChecked(i, false);
+        }
+        filesListBox.EndUpdate();
+    }
+
+    private void generateText_Click(object sender, EventArgs e)
+    {
+
+        if (filesListBox.CheckedItems.Count <= 0)
+        {
+            MessageBox.Show("哎哎哎！你似乎还没有选择文件呢，注意在文件列表右键可以全选哦！", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+
+        if (MessageBox.Show("我们现在开始吗？" + Environment.NewLine +
+                            "本程序使用并行处理字符串，速度应该会非常迅速！ヾ(^▽^*)))" + Environment.NewLine +
+                            "在进行过程中的时候请不要误认为程序卡死哦！w(ﾟДﾟ)w", ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Question) != DialogResult.OK)
+        {
+            return;
+        }
+
+        this.Cursor = Cursors.WaitCursor;
+        this.Enabled = false;
+
+        if (!(templateBox.Text.Contains("%F") || templateBox.Text.Contains("%N")))
+        {
+            MessageBox.Show("模板必须包含%F或者%N占位符！ヾ(≧へ≦)〃", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            this.Enabled = true;
+            this.Cursor = Cursors.Default;
+            return;
+        }
+
+        // 声明一个列表来保存拼接后的文件路径
+        List<string> filePathList = new List<string>();
+
+        // 使用并行处理来拼接文件名和路径
+        Parallel.ForEach(filesListBox.CheckedItems.Cast<object>(), (item) =>
+        {
+            // 这里需要将item转换成字符串，并拼接路径
+            var filePath = Path.Combine(path, item.ToString());
+
+            // 然后将拼接后的路径添加到列表中
+            lock (filePathList)
+            {
+                filePathList.Add(filePath);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        var template = templateBox.Text;
+
+        // 使用并行处理来替换模板中的占位符，并添加到StringBuilder中
+        Parallel.ForEach(filePathList, (filePath) =>
+        {
+            var replacedTemplate = template.Replace("%N", Path.GetFileNameWithoutExtension(filePath)).Replace("%F", filePath);
+            lock (sb)
+            {
+                sb.AppendLine(replacedTemplate);
+            }
+        });
+
+        // 将StringBuilder的内容赋值给result变量
+        var result = sb.ToString();
+
+        this.Enabled = true;
+        this.Cursor = Cursors.Default;
+
+        MessageBox.Show("生成完成！q(≧▽≦q)", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        if (!string.IsNullOrWhiteSpace(result))
+        {
+            Clipboard.SetText(result);
+            MessageBox.Show("已将结果复制到剪贴板！(/≧▽≦)/", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        if (MessageBox.Show("是否将结果保存到一个文件中? ヾ(•ω•`)o", ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "文本文件 (*.txt)|*.txt|批处理文件 (*.bat)|*.bat|所有文件 (*.*)|*.*";
+            saveFileDialog.FileName = "Result.txt"; // 默认文件名
+            saveFileDialog.Title = "保存结果";
+            
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = saveFileDialog.FileName;
+                if (Path.GetExtension(filePath) == ".bat") {
+                    MessageBox.Show("温馨提醒：请注意批处理文件的编码问题哦！记得自己转换哦！", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                File.WriteAllText(filePath, result);
+            }
+        }
+
+        
+    }
+
+}
